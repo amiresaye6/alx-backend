c512630d6e3008da92e88b260e22cf7d16bede3d
Amir Alsayed <amyralsyd367@gmail.com>
2024-05-03 21:30:40 +0300

    fixing pycodestyle

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -57,46 +57,21 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """
-            find the correct indexes to paginate the dataset correctly
-            Args:
-                page: (int) page number
-                page_size: (int) number of records in one page
-
-            Returns:
-                the appropriate page of the dataset
-                (i.e. the correct list of rows).
-                If the input arguments are out of range for the dataset,
-                an empty list should be returned.
-            """
-            assert isinstance(page, int) and isinstance(page_size, int),"!int"
-            assert page > 0 and page_size > 0, "must be greater than 0"
-            data = self.dataset()
-            # print(self.__dataset[1:5])
-            start_index, end_index = index_range(page, page_size)
-            # print(start_index, end_index)
-            return data[start_index: end_index]
-
-
-
-server = Server()
-
-try:
-    should_err = server.get_page(-10, 2)
-except AssertionError:
-    print("AssertionError raised with negative values")
-
-try:
-    should_err = server.get_page(0, 0)
-except AssertionError:
-    print("AssertionError raised with 0")
-
-try:
-    should_err = server.get_page(2, 'Bob')
-except AssertionError:
-    print("AssertionError raised when page and/or page_size are not ints")
-
-
-print(server.get_page(1, 3))
-print(server.get_page(3, 2))
-print(server.get_page(3000, 100))
+        """
+        find the correct indexes to paginate the dataset correctly
+        Args:
+            page: (int) page number
+            page_size: (int) number of records in one page
+        Returns:
+            the appropriate page of the dataset
+            (i.e. the correct list of rows).
+            If the input arguments are out of range for the dataset,
+            an empty list should be returned.
+        """
+        assert isinstance(page, int) and isinstance(page_size, int), "!int"
+        assert page > 0 and page_size > 0, "must be greater than 0"
+        data = self.dataset()
+        # print(self.__dataset[1:5])
+        start_index, end_index = index_range(page, page_size)
+        # print(start_index, end_index)
+        return data[start_index: end_index]
